2366f75bc2da3c0a91cde16961ce0be92c362628
M.Harsha Vardhan <mharsha18vardhan@gmail.com>
2026-07-23 18:26:10 +0530

    Fifth week commit

--- 5.trainersapp/src/TrainersMock.js
+++
@@ -1,7 +0,0 @@
-import { Trainer } from './trainer';
-
-export const trainersMock = [
-    new Trainer(1, "John Doe", "john.doe@cognizant.com", "9876543210", "React", "SPA, Hooks, Redux"),
-    new Trainer(2, "Jane Smith", "jane.smith@cognizant.com", "8765432109", "Java", "Spring Boot, Microservices"),
-    new Trainer(3, "Sam Wilson", "sam.wilson@cognizant.com", "7654321098", "Python", "Data Science, Machine Learning")
-];
